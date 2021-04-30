1592b7bfc65c9bd8fde752984392ccdc3ee3f75a
ARTHUR LUIZ DA SILVA BORGES <arthur_lb@hotmail.com>
2021-04-30 16:34:39 -0300

    funcionamento das regras de negocio

--- scripts/interface.js
+++ scripts/interface.js
@@ -11,19 +11,17 @@ function handleClick(event){
     let square = event.target;
     let position = square.id;
 
-    handleMove(position);
-    updateSquares();
+    if(handleMove(position)){
+        setTimeout(() => {
+            alert("O jogo acabou - o vencendor foi "+playerTime);
+        }, 10);
+    };
+
+    updateSquare(position);
 }
 
-function updateSquares(){
-    let squares = document.querySelectorAll(".square");
-
-    squares.forEach((square) => {
-        let position = square.id;
-        let symbol = board[position];
-
-        if(symbols != ''){
-            square.innerHTML = `<div class='${symbol}'></div>`
-        }
-    })
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`;
 }
